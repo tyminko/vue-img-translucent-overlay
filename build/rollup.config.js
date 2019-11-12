a828741ea67eaebf43e82bcb2303c6bf4e3b8f90
maxim <tyminko@gmail.com>
2019-11-12 16:12:41 +0100

    add component & dev dependencies

---
+++ build/rollup.config.js
@@ -0,0 +1,19 @@
+import commonjs from 'rollup-plugin-commonjs' // Convert CommonJS modules to ES6
+import vue from 'rollup-plugin-vue' // Handle .vue SFC files
+import buble from 'rollup-plugin-buble' // Transpile/polyfill with reasonable browser support
+export default {
+  input: 'src/index.js', // Path relative to package.json
+  output: {
+    name: 'ImgTransOverlay',
+    exports: 'named'
+  },
+  plugins: [
+    commonjs(),
+    vue({
+      css: true, // Dynamically inject css as a <style> tag
+      // @ts-ignore
+      compileTemplate: true // Explicitly convert template to render function
+    }),
+    buble() // Transpile to ES5
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,32 @@
+// Import vue component
+// @ts-ignore
+import component from './ImgTransOverlay.vue'
+
+// Declare install function executed by Vue.use()
+export function install (Vue) {
+  // @ts-ignore
+  if (install.installed) return
+  // @ts-ignore
+  install.installed = true
+  Vue.component('ImgTransOverlay', component)
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+  install
+}
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  // @ts-ignore
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+// To allow use as module (npm/webpack/etc.) export component
+export default component
